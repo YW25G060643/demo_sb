884bf95735bffdce4d86c7fa49d337a562d531a2
Xinshun Wu <yw25g.060643@gmail.com>
2025-03-03 18:04:19 +0800

    250303

--- bc_coin/src/main/java/com/coin/bc_coin/config/RestTemplateConfig.java
+++ bc_coin/src/main/java/com/coin/bc_coin/config/RestTemplateConfig.java
@@ -10,24 +10,6 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplateBuilder()
-            .additionalInterceptors((request, body, execution) -> {
-                int retry = 0;
-                while (retry > 3) {
-                    try {
-                      return execution.execute(request, body);
-                    } catch (ResourceAccessException e) {
-                      retry++;
-                      try {
-                          Thread.sleep(1000);
-                      } catch (InterruptedException ie) {
-                          Thread.currentThread().interrupt();
-                          throw new RuntimeException("Thread was interrupted", ie);
-                      }
-                    }
-                }
-                throw new RuntimeException("Failed to require API");
-            })
-            .build();
+        return new RestTemplate();
     }
 }
--- bc_coin/src/main/java/com/coin/bc_coin/controller/CryptoController.java
+++ bc_coin/src/main/java/com/coin/bc_coin/controller/CryptoController.java
@@ -1,5 +1,6 @@
 package com.coin.bc_coin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.coin.bc_coin.service.CryptoService;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 @Controller
 @RequiredArgsConstructor
 public class CryptoController {
+    @Autowired
     private final CryptoService cryptoService;
 
     @GetMapping("/CryptoInfo")
